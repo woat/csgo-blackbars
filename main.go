bf2f9338cee766a749fb2819346b628406ab47c6
ahmad <ahmad@navlys.com>
2018-07-21 22:03:30 -0400

    formatting

--- main.go
+++ main.go
@@ -12,6 +12,23 @@ import (
 var csgoRes string
 var originRes string
 
+func main() {
+	if len(os.Args) < 2 {
+		nvErr := checkForNvidia()
+		sErr := checkForSteam()
+		if nvErr == nil && sErr == nil {
+			fmt.Println("Requirements have been met.")
+			fmt.Println("Make sure to write your settings in ~/.csbb")
+			os.Exit(3)
+		}
+	}
+
+	getConfigSettings()
+	applyCsgoRes(csgoRes)
+	startCsgo()
+	applyOriginRes(originRes)
+}
+
 func checkForNvidia() error {
 	_, nvErr := exec.LookPath("nvidia-settings")
 	if nvErr != nil {
@@ -30,19 +47,6 @@ func checkForSteam() error {
 	return nil
 }
 
-func applyCsgoRes(arg string) {
-	exec.Command(`nvidia-settings`, `--assign`, `currentmetamode=`+arg).Run()
-}
-
-func applyOriginRes(arg string) {
-	exec.Command(`nvidia-settings`, `--assign`, `currentmetamode=`+arg).Run() 
-}
-
-func startCsgo() {
-	args := os.Args[1:]
-	exec.Command(args[0], args...).Run()
-}
-
 func getConfigSettings() {
 	u, _ := user.Current()
 	f, _ := os.Open(u.HomeDir + "/.csbb")
@@ -56,32 +60,16 @@ func getConfigSettings() {
 	originRes = settings[1]
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		nvErr := checkForNvidia()
-		sErr := checkForSteam()
-		if nvErr == nil && sErr == nil {
-			fmt.Println("Requirements have been met.")
-			fmt.Println("Make sure to write your settings in ~/.csbb")
-			os.Exit(3)
-		}
-	}
-
-
-	getConfigSettings()
-	
-	u, _ := user.Current()
-	f, _ := os.Open(u.HomeDir + "/.csbb")
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	settings := []string{}
-	for scanner.Scan() {
-		settings = append(settings, scanner.Text())
-	}
-
-	csgoRes = settings[0]
-	originRes = settings[1]
-	applyCsgoRes(csgoRes)
-	startCsgo()
-	applyOriginRes(originRes)
+func applyCsgoRes(arg string) {
+	exec.Command(`nvidia-settings`, `--assign`, `currentmetamode=`+arg).Run()
 }
+
+func applyOriginRes(arg string) {
+	exec.Command(`nvidia-settings`, `--assign`, `currentmetamode=`+arg).Run() 
+}
+
+func startCsgo() {
+	args := os.Args[1:]
+	exec.Command(args[0], args...).Run()
+}
+
